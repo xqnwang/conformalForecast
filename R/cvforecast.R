4077c5eddf187411c72510a7a72f72c11a019bf8
dani <daniele.girolimetto@unipd.it>
2026-08-02 16:00:05 +0200

    Check forecast function

--- R/cvforecast.R
+++ R/cvforecast.R
@@ -91,16 +91,34 @@
 #'                       initial = 1, window = 50)
 #'
 #' @export
-cvforecast <- function(y, forecastfun, h = 1, level = c(80, 95),
-                       forward = TRUE, xreg = NULL, initial = 1, window = NULL, ...) {
+cvforecast <- function(
+  y,
+  forecastfun,
+  h = 1,
+  level = c(80, 95),
+  forward = TRUE,
+  xreg = NULL,
+  initial = 1,
+  window = NULL,
+  ...
+) {
   # Check whether there are non-existent arguments
   all.args <- names(formals())
   user.args <- names(match.call())[-1L] # including arguments passed to 3 dots
   check <- user.args %in% all.args
   if (!all(check)) {
     error.args <- user.args[!check]
-    warning(sprintf("The non-existent %s arguments will be ignored.", error.args))
+    warning(sprintf(
+      "The non-existent %s arguments will be ignored.",
+      error.args
+    ))
   }
+
+  # Check forecast function
+  if (missing(forecastfun)) {
+    stop("forecastfun is missing, with no default")
+  }
+  forecastfun <- match.fun(forecastfun)
 
   # Check input univariate time series
   if (any(class(y) %in% c("data.frame", "list", "matrix", "mts"))) {
@@ -121,38 +139,56 @@ cvforecast <- function(y, forecastfun, h = 1, level = c(80, 95),
   }
 
   # Check other inputs
-  if (h <= 0)
+  if (h <= 0) {
     stop("forecast horizon out of bounds")
-  if (initial < 1 | initial > n)
+  }
+  if (initial < 1 | initial > n) {
     stop("initial period out of bounds")
-  if (initial == n && !forward)
+  }
+  if (initial == n && !forward) {
     stop("initial period out of bounds")
+  }
   if (!is.null(window)) {
-    if (window < 1 | window > n)
+    if (window < 1 | window > n) {
       stop("window out of bounds")
-    if (window == n && !forward)
+    }
+    if (window == n && !forward) {
       stop("window out of bounds")
+    }
   }
   if (!is.null(xreg)) {
-    if(!is.numeric(xreg))
+    if (!is.numeric(xreg)) {
       stop("xreg should be a numeric matrix or a numeric vector")
-    xreg <- ts(as.matrix(xreg),
-               start = start(y),
-               frequency = frequency(y))
+    }
+    xreg <- ts(as.matrix(xreg), start = start(y), frequency = frequency(y))
 
-    if (nrow(xreg) < n)
+    if (nrow(xreg) < n) {
       stop("xreg should be at least of the same size as y")
-    if (nrow(xreg) < n + h - !forward)
+    }
+    if (nrow(xreg) < n + h - !forward) {
       # Pad xreg with NAs
-      xreg <- ts(rbind(xreg, matrix(NA, nrow=n+h-!forward-nrow(xreg), ncol=ncol(xreg))),
-                 start = start(y),
-                 frequency = frequency(y))
+      xreg <- ts(
+        rbind(
+          xreg,
+          matrix(NA, nrow = n + h - !forward - nrow(xreg), ncol = ncol(xreg))
+        ),
+        start = start(y),
+        frequency = frequency(y)
+      )
+    }
     if (nrow(xreg) > n + h - !forward) {
-      warning(sprintf("only first %s rows of xreg are being used", n + h - !forward))
+      warning(sprintf(
+        "only first %s rows of xreg are being used",
+        n + h - !forward
+      ))
       xreg <- subset(xreg, start = 1L, end = n + h - !forward)
     }
     if (is.null(colnames(xreg))) {
-      colnames(xreg) <- if (ncol(xreg) == 1) "xreg" else paste0("xreg", 1:ncol(xreg))
+      colnames(xreg) <- if (ncol(xreg) == 1) {
+        "xreg"
+      } else {
+        paste0("xreg", 1:ncol(xreg))
+      }
     }
   }
 
@@ -162,13 +198,20 @@ cvforecast <- function(y, forecastfun, h = 1, level = c(80, 95),
   indx <- seq(nfirst, nlast, by = 1L)
   fit_times <- length(indx)
 
-  pf <- err <- `colnames<-` (
-    ts(matrix(NA_real_, nrow = N, ncol = h),
-       start = start(y),
-       frequency = frequency(y)),
-    paste0("h=", 1:h))
-  if (!is.null(level))
-    lower <- upper <- `names<-` (rep(list(pf), length(level)), paste0(level, "%"))
+  pf <- err <- `colnames<-`(
+    ts(
+      matrix(NA_real_, nrow = N, ncol = h),
+      start = start(y),
+      frequency = frequency(y)
+    ),
+    paste0("h=", 1:h)
+  )
+  if (!is.null(level)) {
+    lower <- upper <- `names<-`(
+      rep(list(pf), length(level)),
+      paste0(level, "%")
+    )
+  }
   out <- list(
     x = y
   )
@@ -177,53 +220,73 @@ cvforecast <- function(y, forecastfun, h = 1, level = c(80, 95),
     y_subset <- subset(
       y,
       start = ifelse(is.null(window), 1L, i - window + 1L),
-      end = i)
+      end = i
+    )
 
     if (is.null(xreg)) {
-      fc <- try(suppressWarnings(
-        forecastfun(y_subset, h = h, level = level, ...)
-        ), silent = TRUE)
+      fc <- try(
+        suppressWarnings(
+          forecastfun(y_subset, h = h, level = level, ...)
+        ),
+        silent = TRUE
+      )
     } else {
       xreg_subset <- subset(
         xreg,
         start = ifelse(is.null(window), 1L, i - window + 1L),
-        end = i)
+        end = i
+      )
       xreg_future <- subset(
         xreg,
         start = i + 1L,
-        end = i + h)
-      fc <- try(suppressWarnings(
-        forecastfun(y_subset, h = h, level = level,
-                    xreg = xreg_subset, newxreg = xreg_future, ...)
-        ), silent = TRUE)
+        end = i + h
+      )
+      fc <- try(
+        suppressWarnings(
+          forecastfun(
+            y_subset,
+            h = h,
+            level = level,
+            xreg = xreg_subset,
+            newxreg = xreg_future,
+            ...
+          )
+        ),
+        silent = TRUE
+      )
     }
 
     if (!is.element("try-error", class(fc))) {
-      pf[i,] <- fc$mean
-      err[i,] <- y[i + 1:h] - fc$mean
+      pf[i, ] <- fc$mean
+      err[i, ] <- y[i + 1:h] - fc$mean
       if (!is.null(level)) {
         for (l in level) {
           levelname <- paste0(l, "%")
-          lower[[levelname]][i,] <- fc$lower[, levelname]
-          upper[[levelname]][i,] <- fc$upper[, levelname]
+          lower[[levelname]][i, ] <- fc$lower[, levelname]
+          upper[[levelname]][i, ] <- fc$upper[, levelname]
         }
       }
     }
   }
 
   out$series <- seriesname
-  if (!is.null(xreg)) out$xreg <- xreg
+  if (!is.null(xreg)) {
+    out$xreg <- xreg
+  }
   out$method <- paste("cvforecast")
   out$fit_times <- fit_times
   out$MEAN <- lagmatrix(pf, 1:h) |> window(start = time(pf)[nfirst + 1L])
-  out$ERROR <- lagmatrix(err, 1:h) |> window(start = time(err)[nfirst + 1L], end = time(err)[n])
+  out$ERROR <- lagmatrix(err, 1:h) |>
+    window(start = time(err)[nfirst + 1L], end = time(err)[n])
   if (!is.null(level)) {
-    out$LOWER <- lapply(lower,
-                        function(low) lagmatrix(low, 1:h) |>
-                          window(start = time(low)[nfirst + 1L]))
-    out$UPPER <- lapply(upper,
-                        function(up) lagmatrix(up, 1:h) |>
-                          window(start = time(up)[nfirst + 1L]))
+    out$LOWER <- lapply(lower, function(low) {
+      lagmatrix(low, 1:h) |>
+        window(start = time(low)[nfirst + 1L])
+    })
+    out$UPPER <- lapply(upper, function(up) {
+      lagmatrix(up, 1:h) |>
+        window(start = time(up)[nfirst + 1L])
+    })
     out$level <- level
   }
   out$call <- match.call()
@@ -252,8 +315,13 @@ print.cvforecast <- function(x, ...) {
     cat(paste("\n"))
   }
 
-  cat(paste("", "fit_times =", x$fit_times,
-            ifelse("model" %in% names(x), "(the forward step included)", ""), "\n"))
+  cat(paste(
+    "",
+    "fit_times =",
+    x$fit_times,
+    ifelse("model" %in% names(x), "(the forward step included)", ""),
+    "\n"
+  ))
 
   if ("model" %in% names(x)) {
     cat(paste("\nForecasts of the forward step:\n"))
@@ -272,7 +340,11 @@ print.summary.cvforecast <- function(x, ...) {
   NextMethod()
   cat("\nCross-validation error measures:\n")
   print(round(
-    accuracy(x, measures = c(point_measures, interval_measures),
-                     byhorizon = FALSE),
-    digits = 3))
+    accuracy(
+      x,
+      measures = c(point_measures, interval_measures),
+      byhorizon = FALSE
+    ),
+    digits = 3
+  ))
 }
